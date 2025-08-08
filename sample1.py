e4406d902015bd4d031fe9ec570e7584d9aa6980
Shaun Figueiro <figueiroshaun@gmail.com>
2025-08-08 17:43:21 -0400

    Added bubble sort code for test

--- sample1.py
+++ sample1.py
@@ -8,3 +8,23 @@ def random_number(min_value, max_value):
 
 random_num = random_number(1, 100)
 print(f"Random number generated: {random_num}")
+
+
+#Bubble sort implementation
+def bubble_sort(arr):
+    n = len(arr)
+    swapped = False
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:   
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        
+        if swapped == False:
+            break
+    return arr
+sample_array = [64, 34, 25, 12, 22, 11, 90]
+sorted_array = bubble_sort(sample_array)            
+
+print(f"Sorted array: {sorted_array}")
+            
